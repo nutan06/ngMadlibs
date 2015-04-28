907bd92a7b8ba9a91ce48bd193e3e10a2f74030f
nutan06 <nutan.mandale@gmail.com>
2015-04-28 08:43:39 -0700

    refractor 1

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('myApp',[]).controller('myController',function($scope){
+	$scope.template=[{
+		femaleName:'female name',
+		jobTitle:'job title',
+		tediousTask:'tedious task',
+		dirtyTask:'dirty task',
+		celebraty:'celebraty',
+		uselessSkill:'useless skill',
+		adjective:'adjective',
+		obnoxiousCelebraty:'obnoxious celebraty',
+		num:'huge number',
+
+	}];
+});
